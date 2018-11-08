c13d0972f03cd398d53e279a359c3f9e88dbddfc
Mahdi Soltan Mohammadi <kingmahdi@email.arizona.edu>
2018-11-08 00:59:42 -0700

    code updates

--- performanceDrivers/fs_csc_inspector.h
+++ performanceDrivers/fs_csc_inspector.h
@@ -20,7 +20,7 @@ void fs_csc_inspector(int n, int* Lp, int* Li, std::vector<std::vector<int>> &DA
   for(In_2 = 0; In_2 < n; In_2++){
     for(In_4 = Lp[In_2]; In_4 < Lp[In_2+1]; In_4++){
       Out_2 = Li[In_4]; 
-      if( In_2 <= Out_2){  //Mehdi, I replaced '<' with '<='
+      if( In_2 < Out_2){ 
         connect(In_2,Out_2, DAG);
       }
     }
