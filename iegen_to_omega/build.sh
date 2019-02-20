4b3253065549532b4320618d60d6e43fd4bba1a6
Eddie Davis <eddiedavis@boisestate.edu>
2019-02-20 09:19:08 -0700

    Added executable.

--- iegen_to_omega/build.sh
+++ iegen_to_omega/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-g++ iegen_to_omega.cc $OMEGAHOME/parser/parser.tab.cc \
-    $OMEGAHOME/parser/lex.yy.cc $OMEGAHOME/parser/AST.cc \
+g++ iegen_to_omega.cc ./parser/parser.tab.cc \
+    ./parser/lex.yy.cc ./parser/AST.cc \
     ${IEGENHOME}/build/src/libiegenlib.a ${IEGENHOME}/lib/installed/lib/libisl.a \
     ${OMEGAHOME}/basic/libomega_basic.a \
     ${OMEGAHOME}/omega_lib/libomega_lib.a ${OMEGAHOME}/code_gen/libcodegen.a \
